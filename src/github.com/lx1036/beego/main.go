c1be571bb823a71569926311c01b42c49a925e2d
Xiang Liu <liuxiang3@360.cn>
2019-09-05 16:45:45 +0800

    (go)struct,method

--- src/github.com/lx1036/beego/main.go
+++ src/github.com/lx1036/beego/main.go
@@ -6,13 +6,13 @@ import (
 
 /*
 https://github.com/golang/go/wiki/Modules#how-do-i-use-vendoring-with-modules-is-vendoring-going-away
- */
-func main()  {
+*/
+func main() {
 	array := [5]int{1, 2, 3, 4, 5}
 	slice := []string{"a", "b", "c"}
 	maps := map[string]int{"a": 1, "b": 2}
 	makeSlice := make([]int, 5)
-	for i := 0; i < 5;i++  {
+	for i := 0; i < 5; i++ {
 		makeSlice[i] = i + 1
 	}
 	makeSlice = append(makeSlice, 10)
--- src/github.com/lx1036/beego/struct_interface_test.go
+++ src/github.com/lx1036/beego/struct_interface_test.go
@@ -1,33 +1,62 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func Perimeter(width float64, height float64) float64 {
-	return (width + height) * 2
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.width + rectangle.height) * 2
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
-}
+/*func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
+}*/
 
 type Rectangle struct {
-	width float64
+	width  float64
 	height float64
 }
 
-func TestPerimeter(t *testing.T)  {
-	got := Perimeter(10.0, 10.0)
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.width * rectangle.height
+}
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want) // .2 表示输出两位小数, f 表示 float64
 	}
 }
 
-func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+type Circle struct {
+	radius float64
+}
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+func TestArea(t *testing.T) {
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
