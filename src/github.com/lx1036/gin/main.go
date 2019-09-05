f193e6fb9519e93d4d55f0d3d0c36a2b535aaed6
Xiang Liu <lx1036@126.com>
2019-09-06 00:03:00 +0800

    (net)http

--- src/github.com/lx1036/gin/main.go
+++ src/github.com/lx1036/gin/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
 )
 
@@ -11,9 +12,9 @@ type Person struct {
 	Age int
 }
 
-func main()  {
-	app := gin.Default()
-	routes := app.GET("/ping", func(context *gin.Context) {
+func ginDemo()  {
+	engine := gin.Default()
+	iRoutes := engine.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"message": Person{
 				Name: "lx1036",
@@ -22,14 +23,36 @@ func main()  {
 		})
 	})
 
-	routes.GET("/hello", func(context *gin.Context) {
+	iRoutes.GET("/hello", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"data": "world",
 		})
 	})
-	
-	err := app.Run(":8080")
+
+	routes := engine.Routes()
+	for _, route := range routes {
+		fmt.Printf("Method: %s Path: %s Handler: %s HandlerFunc: %T \n", route.Method, route.Path, route.Handler, route.HandlerFunc)
+	}
+
+	err := engine.Run(":8080")
 	if err != nil {
 		fmt.Printf("uncaught error: %v", err)
 	}
+}
+
+func pkgNetHttpDemo()  {
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		io.WriteString(writer, fmt.Sprintf("world from %s %s", request.Method, request.URL))
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
+
+/*
+https://golang.org/pkg/net/http/
+ */
+func main()  {
+	//ginDemo()
+
+	pkgNetHttpDemo()
 }
