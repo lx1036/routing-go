b824e2c0fd269b1e0bdb74785acb304162bbcb69
Xiang Liu <lx1036@126.com>
2019-09-08 23:40:08 +0800

    (algorithm)radix tree

---
+++ src/github.com/lx1036/app/framework/algorithm/redix_tree_test.go
@@ -0,0 +1,12 @@
+package algorithm
+
+import "testing"
+
+func TestTrie(test *testing.T) {
+	t0 := New()
+	if t0.Search([]byte{0}) != nil {
+		test.Errorf("Trie contains data for missing key")
+	}
+
+	t1 := t0.Insert([]byte{0}, []byte("test"))
+}
